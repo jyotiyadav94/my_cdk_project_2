ba03e93d105d1df0775c158881726029042b89ef
Jyoti Yadav <72126242+jyotiyadav94@users.noreply.github.com>
2024-08-10 12:45:43 +0200

    Add files via upload

---
+++ test/my-cdk-project2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MyCdkProject2 from '../lib/my-cdk-project2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/my-cdk-project2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MyCdkProject2.MyCdkProject2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
